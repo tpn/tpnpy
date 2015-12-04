a60811c6cf3c4e94d09c8d1b80e3281e4b5af55b
Trent Nelson <trent@trent.me>
2015-12-04 12:39:06 -0500

    Implement initial implementation of VsProfileProgram command.

--- lib/tpn/commands.py
+++ lib/tpn/commands.py
@@ -13,7 +13,15 @@ from .command import (
 )
 
 from .invariant import (
+    BoolInvariant,
+    PathInvariant,
+    StringInvariant,
+    DirectoryInvariant,
     PositiveIntegerInvariant,
+)
+
+from .commandinvariant import (
+    InvariantAwareCommand,
 )
 
 #===============================================================================
@@ -104,5 +112,135 @@ class BitsTable3(Command):
     def run(self):
         from .util import bits_table3
         self._out(bits_table3())
+
+class VsProfileProgram(InvariantAwareCommand):
+    """
+    Runs the vspyprof against the given file.
+    """
+    _verbose_ = True
+
+    python_file = None
+    _python_file = None
+    class PythonFileArg(PathInvariant):
+        _help = "Path to the Python file to profile"
+        _mandatory = True
+
+    python_exe = None
+    _python_exe = None
+    class PythonExeArg(PathInvariant):
+        _help = (
+            "Path to the Python interpreter to use.  Defaults to "
+            "[ptvs:python_exe], or if that's not set, whatever "
+            "sys.executable is."
+        )
+        _mandatory = False
+
+    program_args = None
+    class ProgramArgsArg(StringInvariant):
+        _help = (
+            "Additional list of arguments to pass to the Python "
+            "file being profiled.  (XXX: Not Yet Implemented.)"
+        )
+        _mandatory = False
+
+    vspyprof_dll = None
+    class VspyprofDllArg(PathInvariant):
+        _help = (
+            "Path to the Visual Studio Profiler DLL "
+            "(defaults to [ptvs:dll] in config file)."
+        )
+        _mandatory = False
+
+    use_debug_dlls = None
+    class UseDebugDllsArg(BoolInvariant):
+        _help = "Use the debug versions of profiler DLLs."
+        _mandatory = False
+        _default = False
+
+    custom_profiler_dll = None
+    class CustomProfilerDllArg(PathInvariant):
+        _help = (
+            "Optional path to a custom profile DLL to use instead of the "
+            "Visual Studio vsperf.dll that vspyprof.dll was built against. "
+            "This must export C __stdcall symbols for EnterFunction, "
+            "ExitFunction, NameToken and SourceLine.  (See PythonApi.cpp "
+            "in PTVS for more info.)"
+        )
+        _mandatory = False
+
+    pause_before_starting = None
+    class PauseBeforeStartingArg(BoolInvariant):
+        _help = (
+            "If set to true, pauses prior to starting profiling.  This allows "
+            "you to independently attach debuggers, etc."
+        )
+        _default = False
+        _mandatory = False
+
+    run_dir = None
+    _run_dir = None
+    class RunDirArg(DirectoryInvariant):
+        _help = "Directory to run the profiler from."
+
+    def run(self):
+        InvariantAwareCommand.run(self)
+
+        conf = self.conf
+        dll = self.options.profiler_dll
+        if not dll:
+            if self.options.use_debug_dlls:
+                dll = conf.ptvs_debug_dll_path
+            else:
+                dll = conf.ptvs_dll_path
+
+        custdll = self.options.custom_profiler_dll
+        if not custdll:
+            if self.options.use_debug_dlls:
+                custdll = conf.ptvs_custom_debug_dll_path
+            else:
+                custdll = conf.ptvs_custom_dll_path
+        self._verbose("Using profiler DLL: %s" % dll)
+        if custdll:
+            self._verbose("Using custom profiler DLL: %s" % custdll)
+        else:
+            custdll = '-'
+        from . import vspyprof
+
+        from .util import chdir
+        from .path import join_path
+        from os.path import dirname
+
+        import sys
+
+        this_dir = dirname(__file__)
+        runvspyprof = join_path(this_dir, 'runvspyprof.py')
+
+        exe = self._python_exe or conf.ptvs_python_exe
+
+        cmd = [
+            exe,
+            runvspyprof,
+            dll,
+            self._run_dir,
+            custdll,
+            self._python_file,
+        ]
+
+        program_args = self.options.program_args
+        if program_args:
+            import shlex
+            args = shlex.split(program_args)
+            cmd.append(args)
+
+        if self.options.pause_before_starting:
+            import msvcrt
+            sys.stdout.write('Press any key to continue . . .')
+            sys.stdout.flush()
+            msvcrt.getch()
+
+        import subprocess
+        with chdir(this_dir):
+
+            subprocess.call(cmd)
 
 # vim:set ts=8 sw=4 sts=4 tw=80 et                                             :

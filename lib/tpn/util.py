b9a7268b5b700ca81431379d3cc8425a70b750e9
Trent Nelson <trent@trent.me>
2024-05-01 22:09:02 -0700

    Implement rotate_file, get_openai_key, SortedCollection.
    Tweak Dict and SlotObject.

--- lib/tpn/util.py
+++ lib/tpn/util.py
@@ -4,10 +4,12 @@
 import os
 import re
 import sys
+import copy
 import json
 import time
 import shutil
 import inspect
+import hashlib
 import calendar
 import datetime
 import itertools
@@ -20,6 +22,11 @@ except ImportError:
     import io
     StringIO = io.StringIO
 
+from bisect import (
+    bisect_left,
+    bisect_right,
+)
+
 from datetime import (
     timezone,
     timedelta,
@@ -31,6 +38,7 @@ from os.path import (
     abspath,
     dirname,
     basename,
+    splitext,
     normpath,
 )
 
@@ -704,6 +712,16 @@ if is_linux:
         PR_SET_NAME = 15
         libc.prctl(PR_SET_NAME, name, 0, 0, 0)
 
+    def get_openai_key(path=None):
+        command = ['gpg', '-d', '--batch', '--quiet']
+        if path is None:
+            path = '~/.zsh/openai_key.asc'
+        path = os.path.expanduser(path)
+        if not os.path.exists(path):
+            raise RuntimeError('File not found: %s' % path)
+        command.append(path)
+        return subprocess.check_output(command).strip().decode('utf-8')
+
 class classproperty(property):
     def __get__(self, obj, type_):
         return self.fget.__get__(None, type_)()
@@ -1292,6 +1310,29 @@ def clear_screen():
     else:
         sys.stdout.write(chr(27) + "[2J")
 
+def rotate_file(path, zfill_digits=5):
+    """
+    Rotate a file by renaming it to a new name with a sequential number
+    injected.  The new name will be the original basename (i.e. excluding
+    `.<extension>`, followed by an underscore, and then a zero-padded number.
+    The number of digits to pad with is specified by `zfill_digits`.  The
+    routine will start at 1 and increment until it finds a filename that does
+    not exist.
+
+    If no free file can be found based on `zfill_digits`, the routine will
+    add an additional digit to `zfill_digits` and try again.
+    """
+    (prefix, ext) = splitext(path)
+    base = basename(prefix)
+    while True:
+        for i in itertools.count(1):
+            new_path = f'{base}_{str(i).zfill(zfill_digits)}{ext}'
+            if not os.path.exists(new_path):
+                os.rename(path, new_path)
+                return new_path
+
+        zfill_digits += 1
+
 # memoize/memoized lovingly stolen from conda.utils.
 class memoized(object):
     """Decorator. Caches a function's return value each time it is called.
@@ -1599,10 +1640,13 @@ class chdir(object):
 class SlotObject(object):
     # Subclasses need to define __slots__
     _default_ = None
+    _defaults_ = dict()
 
     _to_dict_prefix_ = ''
     _to_dict_suffix_ = ''
     _to_dict_exclude_ = set()
+    # If set to True, automatically exclude any slots with None values.
+    _to_dict_exclude_none_values_ = False
 
     # Defaults to _to_dict_exclude_ if not set.
     _repr_exclude_ = set()
@@ -1622,7 +1666,11 @@ class SlotObject(object):
 
         for slot in self.__slots__:
             if slot not in seen:
-                setattr(self, slot, self._default_)
+                if slot in self._defaults_:
+                    default = copy.deepcopy(self._defaults_[slot])
+                else:
+                    default = copy.deepcopy(self._default_)
+                setattr(self, slot, default)
 
         return
 
@@ -1630,11 +1678,14 @@ class SlotObject(object):
         prefix = prefix or self._to_dict_prefix_
         suffix = suffix or self._to_dict_suffix_
         exclude = exclude or self._to_dict_exclude_
-        return {
-            '%s%s%s' % (prefix, key, suffix): getattr(self, key)
+        d = {
+            f'{prefix}{key}{suffix}': getattr(self, key)
                 for key in self.__slots__
-                     if key not in exclude
+                    if key not in exclude
         }
+        if self._to_dict_exclude_none_values_:
+            d = { k: v for (k, v) in d.items() if v is not None }
+        return d
 
     def __repr__(self):
         slots = self.__slots__
@@ -1652,6 +1703,34 @@ class SlotObject(object):
                     )
                 )
         )
+
+    @property
+    def sha256(self):
+        """Compute a SHA256 checksum of all key-value pairs in the dictionary."""
+        hasher = hashlib.sha256()
+        # Sort the keys to ensure consistent order
+        sorted_items = sorted(self._to_dict().items())
+        # Create a consistent string representation of the dictionary
+        encoded_items = str(sorted_items).encode()
+        # Update the hash with this byte string
+        hasher.update(encoded_items)
+        # Return the hexadecimal string representation of the hash
+        return hasher.hexdigest()
+
+    @property
+    def sha256_path(self):
+        """
+        Return a path-based SHA256 checksum similar to git's object storage.
+        The first two characters of the checksum are used as a directory,
+        and the remaining characters are used as the filename.
+        """
+        checksum = self.sha256
+        return f'{checksum[:2]}/{checksum[2:]}'
+
+    def __hash__(self):
+        """Override the hash function to use the SHA256 checksum."""
+        # Convert the hex digest to an integer
+        return int(self.sha256, 16)
 
 class UnexpectedCodePath(RuntimeError):
     pass
@@ -1852,6 +1931,24 @@ class Dict(dict):
         return self.__getitem__(name)
     def __setattr__(self, name, value):
         return self.__setitem__(name, value)
+
+    @property
+    def sha256(self):
+        """Compute a SHA256 checksum of all key-value pairs in the dictionary."""
+        hasher = hashlib.sha256()
+        # Sort the keys to ensure consistent order
+        sorted_items = sorted(self.items())
+        # Create a consistent string representation of the dictionary
+        encoded_items = str(sorted_items).encode()
+        # Update the hash with this byte string
+        hasher.update(encoded_items)
+        # Return the hexadecimal string representation of the hash
+        return hasher.hexdigest()
+
+    def __hash__(self):
+        """Override the hash function to use the SHA256 checksum."""
+        # Convert the hex digest to an integer
+        return int(self.sha256, 16)
 
 class DecayDict(Dict):
     """
@@ -2345,6 +2442,199 @@ if os.name == 'nt':
                 if not cls or wcls.lower().startswith(cls.lower()):
                     print("setting '%s' (%d, %s) on top" % (wtext, hwnd, cls))
                     SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, flags)
+
+# From: https://code.activestate.com/recipes/577197-sortedcollection/
+class SortedCollection(object):
+    '''Sequence sorted by a key function.
+
+    SortedCollection() is much easier to work with than using bisect() directly.
+    It supports key functions like those use in sorted(), min(), and max().
+    The result of the key function call is saved so that keys can be searched
+    efficiently.
+
+    Instead of returning an insertion-point which can be hard to interpret, the
+    five find-methods return a specific item in the sequence. They can scan for
+    exact matches, the last item less-than-or-equal to a key, or the first item
+    greater-than-or-equal to a key.
+
+    Once found, an item's ordinal position can be located with the index() method.
+    New items can be added with the insert() and insert_right() methods.
+    Old items can be deleted with the remove() method.
+
+    The usual sequence methods are provided to support indexing, slicing,
+    length lookup, clearing, copying, forward and reverse iteration, contains
+    checking, item counts, item removal, and a nice looking repr.
+
+    Finding and indexing are O(log n) operations while iteration and insertion
+    are O(n).  The initial sort is O(n log n).
+
+    The key function is stored in the 'key' attibute for easy introspection or
+    so that you can assign a new key function (triggering an automatic re-sort).
+
+    In short, the class was designed to handle all of the common use cases for
+    bisect but with a simpler API and support for key functions.
+
+    >>> from pprint import pprint
+    >>> from operator import itemgetter
+
+    >>> s = SortedCollection(key=itemgetter(2))
+    >>> for record in [
+    ...         ('roger', 'young', 30),
+    ...         ('angela', 'jones', 28),
+    ...         ('bill', 'smith', 22),
+    ...         ('david', 'thomas', 32)]:
+    ...     s.insert(record)
+
+    >>> pprint(list(s))         # show records sorted by age
+    [('bill', 'smith', 22),
+     ('angela', 'jones', 28),
+     ('roger', 'young', 30),
+     ('david', 'thomas', 32)]
+
+    >>> s.find_le(29)           # find oldest person aged 29 or younger
+    ('angela', 'jones', 28)
+    >>> s.find_lt(28)           # find oldest person under 28
+    ('bill', 'smith', 22)
+    >>> s.find_gt(28)           # find youngest person over 28
+    ('roger', 'young', 30)
+
+    >>> r = s.find_ge(32)       # find youngest person aged 32 or older
+    >>> s.index(r)              # get the index of their record
+    3
+    >>> s[3]                    # fetch the record at that index
+    ('david', 'thomas', 32)
+
+    >>> s.key = itemgetter(0)   # now sort by first name
+    >>> pprint(list(s))
+    [('angela', 'jones', 28),
+     ('bill', 'smith', 22),
+     ('david', 'thomas', 32),
+     ('roger', 'young', 30)]
+
+    '''
+
+    def __init__(self, iterable=(), key=None):
+        self._given_key = key
+        key = (lambda x: x) if key is None else key
+        decorated = sorted((key(item), item) for item in iterable)
+        self._keys = [k for k, item in decorated]
+        self._items = [item for k, item in decorated]
+        self._key = key
+
+    def _getkey(self):
+        return self._key
+
+    def _setkey(self, key):
+        if key is not self._key:
+            self.__init__(self._items, key=key)
+
+    def _delkey(self):
+        self._setkey(None)
+
+    key = property(_getkey, _setkey, _delkey, 'key function')
+
+    def clear(self):
+        self.__init__([], self._key)
+
+    def copy(self):
+        return self.__class__(self, self._key)
+
+    def __len__(self):
+        return len(self._items)
+
+    def __getitem__(self, i):
+        return self._items[i]
+
+    def __iter__(self):
+        return iter(self._items)
+
+    def __reversed__(self):
+        return reversed(self._items)
+
+    def __repr__(self):
+        return '%s(%r, key=%s)' % (
+            self.__class__.__name__,
+            self._items,
+            getattr(self._given_key, '__name__', repr(self._given_key))
+        )
+
+    def __reduce__(self):
+        return self.__class__, (self._items, self._given_key)
+
+    def __contains__(self, item):
+        k = self._key(item)
+        i = bisect_left(self._keys, k)
+        j = bisect_right(self._keys, k)
+        return item in self._items[i:j]
+
+    def index(self, item):
+        'Find the position of an item.  Raise ValueError if not found.'
+        k = self._key(item)
+        i = bisect_left(self._keys, k)
+        j = bisect_right(self._keys, k)
+        return self._items[i:j].index(item) + i
+
+    def count(self, item):
+        'Return number of occurrences of item'
+        k = self._key(item)
+        i = bisect_left(self._keys, k)
+        j = bisect_right(self._keys, k)
+        return self._items[i:j].count(item)
+
+    def insert(self, item):
+        'Insert a new item.  If equal keys are found, add to the left'
+        k = self._key(item)
+        i = bisect_left(self._keys, k)
+        self._keys.insert(i, k)
+        self._items.insert(i, item)
+
+    def insert_right(self, item):
+        'Insert a new item.  If equal keys are found, add to the right'
+        k = self._key(item)
+        i = bisect_right(self._keys, k)
+        self._keys.insert(i, k)
+        self._items.insert(i, item)
+
+    def remove(self, item):
+        'Remove first occurence of item.  Raise ValueError if not found'
+        i = self.index(item)
+        del self._keys[i]
+        del self._items[i]
+
+    def find(self, k):
+        'Return first item with a key == k.  Raise ValueError if not found.'
+        i = bisect_left(self._keys, k)
+        if i != len(self) and self._keys[i] == k:
+            return self._items[i]
+        raise ValueError('No item found with key equal to: %r' % (k,))
+
+    def find_le(self, k):
+        'Return last item with a key <= k.  Raise ValueError if not found.'
+        i = bisect_right(self._keys, k)
+        if i:
+            return self._items[i-1]
+        raise ValueError('No item found with key at or below: %r' % (k,))
+
+    def find_lt(self, k):
+        'Return last item with a key < k.  Raise ValueError if not found.'
+        i = bisect_left(self._keys, k)
+        if i:
+            return self._items[i-1]
+        raise ValueError('No item found with key below: %r' % (k,))
+
+    def find_ge(self, k):
+        'Return first item with a key >= equal to k.  Raise ValueError if not found'
+        i = bisect_left(self._keys, k)
+        if i != len(self):
+            return self._items[i]
+        raise ValueError('No item found with key at or above: %r' % (k,))
+
+    def find_gt(self, k):
+        'Return first item with a key > k.  Raise ValueError if not found'
+        i = bisect_right(self._keys, k)
+        if i != len(self):
+            return self._items[i]
+        raise ValueError('No item found with key above: %r' % (k,))
 
 
 if __name__ == '__main__':
